d532e3dbde49c9647e6e15ad77537daa8314b18b
Dário Nzita <92880647+DARIONZITA@users.noreply.github.com>
2025-05-25 21:30:43 +0100

    m

--- web/index.php
+++ web/index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,8 +6,17 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Dario nzita</h1>
+    <h1>Dario Nzita</h1>
     <h2>Inception</h2>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod deserunt fugiat doloribus magnam qui quos totam, dicta nesciunt placeat vel voluptatem sunt doloremque optio voluptates iste ipsum explicabo reiciendis sed!Lorem</p>
+
+    <h3>Lista de números de 1 a 5:</h3>
+    <ul>
+    <?php
+        for ($i = 1; $i <= 5; $i++) {
+            echo "<li>Número $i</li>";
+        }
+    ?>
+    </ul>
 </body>
 </html>
